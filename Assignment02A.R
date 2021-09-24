05bb61b6a8199b9a32414bd411e3ab66c812ed5c
RyukXu <89795875+RyukXu@users.noreply.github.com>
2021-09-24 03:44:24 -0400

    Add files via upload

---
+++ Assignment02A.R
@@ -0,0 +1,6 @@
+#Rprogram to find time until end of semester
+
+#difftime function
+var.1 <- difftime(Sys.Date(),"2020-12-3",units= "days")
+cat("Luke, the number of days until the end of the semester is",var.1)
+
