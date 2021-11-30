896d9645c8a9c6a4c0b1bb050d7bc26a131381f3
Luke Xu <lux222@lehigh.edu>
2021-11-30 13:27:21 -0500

    Assignment 05

--- Assignment 05.R
+++ Assignment 05.R
@@ -4,7 +4,7 @@ csv_data <- read.csv("d:\\task\\TBQ05.20211116133909.csv", encoding="UTF-8")
 print(is.data.frame(csv_data))  # 查看是否是数据框
 print(ncol(csv_data))  # 列数
 print(nrow(csv_data))  # 行数
-#View(csv_data)
+View(csv_data) #查看列表
 #head(csv_data)
 #1.将值转换为数字。
 csv_data[10]=as.numeric(unlist(csv_data[10]))
@@ -37,4 +37,22 @@ paste("tonnage$Region.of.Trade: Foreign trade: Other EU " ,colSums(data5[10]) )
 #"Foreign trade: Other ports" 
 data6 <- subset(csv_data, Region.of.Trade=="Foreign trade: Other ports")
 paste("tonnage$Region.of.Trade: Foreign trade: Other ports " ,colSums(data6[10]) )
+
+#4从英国和爱尔兰北部抵达的季度吨位数据生成散点图 
+#过滤港口数据
+data9 <- subset(data3, Port!="All Main Irish Ports")
+#修改列名
+names(data9)[names(data9) == 'VALUE'] <- 'Tonnage'
+#View(data9) 
+#安装colorspace包,只需安装一次
+#install.packages('colorspace', depend=TRUE)
+#导入程序包，如果报错，先执行 install.packages('colorspace', depend=TRUE)
+library("ggplot2")
+
+# 基函数：colour设置分组
+ggplot(data9, aes(x = Quarter, y = Tonnage, colour = Port)) +
+# 生成散点图函数
+geom_point()+
+#生成散点图标题
+ggtitle(label = 'Quartely Tonnage Arriving From Great Britain and Northern Ireland')
 
